fd533726bf170764d91b50680ccc26d8dc9fb625
Administrator <lumengbit2@gmail.com>
2019-03-14 14:28:15 +1100

    Component/Home: add home page and layout

---
+++ src/components/Home/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './../../less/style.less';
+import './home.less';
+
+function Home() {
+    return (
+        <div className="home-page">
+            <div className="results-area">
+                <div className="result">
+                </div>
+            </div>
+            <div className="saved-properties-area">
+                <div className="saved-properties">
+                </div>
+            </div>
+        </div>
+    );
+}
+export default Home;
